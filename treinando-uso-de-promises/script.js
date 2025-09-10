c08a864ec578ca7872f18f29714220efe37b493c
Kevin Tereza <kevinterezadev@gmail.com>
2025-09-10 19:30:33 -0300

    Exercício Refatorando as Promises

--- treinando-uso-de-promises/script.js
+++ treinando-uso-de-promises/script.js
@@ -1,33 +1,30 @@
-function calculateImc(weight, height) {
-    const p = new Promise((resolve, reject) => {
-        if (isNaN(weight) || isNaN(height)) {
-            reject("Os valores informados precisam ser números.")
+async function calculateImc(weight, height) { // Já cria uma new Promise
+    if (isNaN(weight) || isNaN(height)) {
+            return Promise.reject("Os valores informados precisam ser números.") // funciona como reject()
             
         } else {
-            resolve((weight / (height**2)))
+            return (weight / (height**2)) // funciona como resolve()
         }
-    })
-    return p
 }
 
-function printImc(weight, height) {
-    calculateImc(weight, height).then(result => {
-        if (result < 18.5) {
-            console.log(`Abaixo de 18,5: Magreza (IMC = ${result.toFixed(2)})`)
-        } else if (result <= 24.9) {
-            console.log(`Entre 18,5 e 24,9: Normal (IMC = ${result.toFixed(2)})`)
-        } else if (result <= 29.9) {
-            console.log(`Entre 25 e 29,9: Sobrepeso (IMC = ${result.toFixed(2)})`)
-        } else if (result <= 39.9) {
-            console.log(`Entre 30 e 39,9: Obesidade (IMC = ${result.toFixed(2)})`)
-        } else {
-            console.log(`Acima de 40: Obesidade Grave (IMC = ${result.toFixed(2)})`)
-        }
-    }).catch(err => {
-        console.log(err)
-    })
+async function printImc(weight, height) { // Já cria uma new Promise para usar o await
+    try { // Trata como o .then
+        const imcNumber = await calculateImc(weight, height) // espera pelo calculo e retorna o result ou erro
 
-    console.log("Executando depois de chamar a função")
+        if (imcNumber <= 18.5) {
+            console.log(`Abaixo de 18,5: Magreza (IMC = ${imcNumber.toFixed(2)})`)
+        } else if (imcNumber <= 24.9) {
+            console.log(`Entre 18,5 e 24,9: Normal (IMC = ${imcNumber.toFixed(2)})`)
+        } else if (imcNumber <= 29.9) {
+            console.log(`Entre 25 e 29,9: Sobrepeso (IMC = ${imcNumber.toFixed(2)})`)
+        } else if (imcNumber <= 39.9) {
+            console.log(`Entre 30 e 39,9: Obesidade (IMC = ${imcNumber.toFixed(2)})`)
+        } else {
+            console.log(`Acima de 40: Obesidade Grave (IMC = ${imcNumber.toFixed(2)})`)
+        }
+    } catch (err) { // trata como o .catch
+        console.log(err)
+    }
 }
 
 printImc(65, "1.70")   
